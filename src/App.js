82b18fe5e21b38f329e1e2797d04266170c6c617
Fabio Hsu <Fabio90h@Fabios-MacBook-Pro.local>
2021-09-19 22:24:32 -0700

    Initial mock commit with button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button buttonText="hello world" />
     </div>
   );
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  border: 1px solid red;
+  background-color: greenyellow;
+`;
+
+export default Button;
