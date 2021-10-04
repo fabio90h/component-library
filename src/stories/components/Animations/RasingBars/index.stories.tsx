45c356468ed4415927b68987052aea7b85473f1e
Fabio <hsu.fabio13@gmail.com>
2021-10-04 06:21:34 -0700

    Raising bars animations

---
+++ src/stories/components/Animations/RasingBars/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { RasingBar } from '../../../../components/Animations/RisingBars';
+
+export default {
+  title: 'Animations/RasingBar',
+  component: RasingBar,
+};
+
+export const Primary = () => <RasingBar />;
