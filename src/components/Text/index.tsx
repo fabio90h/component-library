49f20f887539e62d5e1865a6fd16c26f1edf0066
Fabio <hsu.fabio13@gmail.com>
2021-10-21 08:54:53 -0700

    Text component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,8 +1,59 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { CssStyleable } from '../../utils/StyledComponents';
 
-const Text = styled.div`
-    font-size: 15px;
-`
+type Props = CssStyleable & {
+  type: 'h1' | 'h2' | 'subheader' | 'p';
+};
 
-export default Text
+const H1 = styled.h1<CssStyleable>`
+  color: ${(props) => props.theme.color};
+  font-family: ${(props) => props.theme.fontFamily};
+  font-size: ${(props) => props.theme.h1.fontSize};
+  line-height: ${(props) => props.theme.h1.lineHeight};
+
+  ${(props) => props.cssStyles};
+`;
+
+const H2 = styled.h2<CssStyleable>`
+  color: ${(props) => props.theme.color};
+  font-family: ${(props) => props.theme.fontFamily};
+  font-size: ${(props) => props.theme.h2.fontSize};
+  line-height: ${(props) => props.theme.h2.lineHeight};
+
+  ${(props) => props.cssStyles};
+`;
+
+const Subheader = styled.h3<CssStyleable>`
+  color: ${(props) => props.theme.color};
+  font-family: ${(props) => props.theme.fontFamily};
+  font-size: ${(props) => props.theme.subheader.fontSize};
+  line-height: ${(props) => props.theme.subheader.lineHeight};
+
+  ${(props) => props.cssStyles};
+`;
+
+const P = styled.p<CssStyleable>`
+  color: ${(props) => props.theme.color};
+  font-family: ${(props) => props.theme.fontFamily};
+  font-size: ${(props) => props.theme.p.fontSize};
+  line-height: ${(props) => props.theme.p.lineHeight};
+
+  ${(props) => props.cssStyles};
+`;
+
+const Text: React.FC<Props> = ({ type, children, cssStyles }) => {
+  switch (type) {
+    case 'h1':
+      return <H1 cssStyles={cssStyles}>{children}</H1>;
+    case 'h2':
+      return <H2 cssStyles={cssStyles}>{children}</H2>;
+    case 'subheader':
+      return <Subheader cssStyles={cssStyles}>{children}</Subheader>;
+    case 'p':
+    default:
+      return <P cssStyles={cssStyles}>{children}</P>;
+  }
+};
+
+export default Text;
--- src/utils/Theme.ts
+++ src/utils/Theme.ts
@@ -1,4 +1,4 @@
-export type BaseTheme = typeof Base
+export type BaseTheme = typeof Base;
 
 const AirBnbRawTheme = {
   /**
@@ -13,58 +13,104 @@ const AirBnbRawTheme = {
   colorLevel7: '#717171',
   colorLevel8: '#484848',
   colorLevel9: '#222222',
-  colorLevel10:'0000000', // black
+  colorLevel10: '0000000', // black
 
   // Used website https://usbrandcolors.com/airbnb-colors/
   // Shades: https://mdigi.tools/color-shades/#00a699
   brandColor1: '#ff9fb1',
   brandColor2: '#ff607c',
-  brandColor3: "#FF385C",
+  brandColor3: '#FF385C',
 
   secondColor1: '#00dccb',
   secondColor2: '#00c5b6',
-  secondColor3: "#00A699",
+  secondColor3: '#00A699',
 
   thirdColor1: '#fd8054',
   thirdColor2: '#fc703d',
-  thirdColor3: "#FC642D",
+  thirdColor3: '#FC642D',
 
   /**
    * BORDER
    */
 
   // Radius
-  borderRadiusTiny: "4px",
-  borderRadiusSmall: "8px",
-  borderRadiusBase: "12px",
-  borderRadiusLarge: "16px",
+  borderRadiusTiny: '4px',
+  borderRadiusSmall: '8px',
+  borderRadiusBase: '12px',
+  borderRadiusLarge: '16px',
 
   /**
    * FONT
    */
-  fontFamily: 'Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif',
-  
-  // Size
-  fontSizeBase: "16px",
-  fontSizeLarge: "26px",
+  fontFamily:
+    'Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif',
 
-  fontWeightBase: "600",
+  // Size
+  fontSizeTiny: '6px',
+  fontSizeSmall: '12px',
+  fontSizeBase: '16px',
+  fontSizeLarge: '26px',
+  fontSizeXLarge: '32px',
+
+  // Weight
+  fontWeightBase: '400',
+  fontWeightHeavy: '600',
+  fontWeightXHeavy: '800',
 
   // Line Height
-  fontLineHeightBase: "20px",
-  fontLineHeightLarge: "30px",
+  fontLineHeightBase: '20px',
+  fontLineHeightLarge: '30px',
+  fontLineHeightXLarge: '36px',
 
   /**
    * SPACING
    */
-  spaceTiny: "8px",
-  spaceSmall: "16px",
-  spaceBase: "24px",
-  spaceLarge: "48px",
-  spaceXlarge: "64px",
-}
+  spaceTiny: '8px',
+  spaceSmall: '16px',
+  spaceBase: '24px',
+  spaceLarge: '48px',
+  spaceXlarge: '64px',
+};
 
 const Base = {
+  /**
+   * GENERAL
+   */
+  fontFamily: AirBnbRawTheme.fontFamily,
+  color: AirBnbRawTheme.colorLevel9,
+
+  /**
+   * H1
+   */
+  h1: {
+    fontSize: AirBnbRawTheme.fontSizeXLarge,
+    lineHeight: AirBnbRawTheme.fontLineHeightXLarge,
+    fontWeight: AirBnbRawTheme.fontWeightXHeavy,
+  },
+  /**
+   * H2
+   */
+  h2: {
+    fontSize: AirBnbRawTheme.fontSizeLarge,
+    lineHeight: AirBnbRawTheme.fontLineHeightLarge,
+    fontWeight: AirBnbRawTheme.fontWeightHeavy,
+  },
+  /**
+   * SUBHEADER
+   */
+  subheader: {
+    fontSize: AirBnbRawTheme.fontSizeBase,
+    lineHeight: AirBnbRawTheme.fontLineHeightBase,
+    fontWeight: AirBnbRawTheme.fontWeightBase,
+  },
+  /**
+   * Paragraph
+   */
+  p: {
+    fontSize: AirBnbRawTheme.fontSizeBase,
+    lineHeight: AirBnbRawTheme.fontLineHeightBase,
+  },
+
   /**
    * BUTTON
    */
@@ -82,7 +128,7 @@ const Base = {
     activeBackgroundColor: AirBnbRawTheme.colorLevel2,
     activeColor: AirBnbRawTheme.colorLevel1,
     activeBorderColor: AirBnbRawTheme.colorLevel10,
-    activeBorder: "none",
+    activeBorder: 'none',
 
     // DISABLE
     disableBorderColor: AirBnbRawTheme.colorLevel4,
@@ -104,16 +150,18 @@ const Base = {
    * DOT
    */
   dot: {
-    backgroundColor: AirBnbRawTheme.colorLevel9
-  }
-}
+    backgroundColor: AirBnbRawTheme.colorLevel9,
+  },
+};
 
 export const LightTheme: BaseTheme = {
-  ...Base
-}
+  ...Base,
+};
 
 export const DarkTheme: BaseTheme = {
   ...Base,
+
+  color: AirBnbRawTheme.colorLevel1,
 
   /**
    * BUTTON
@@ -124,7 +172,7 @@ export const DarkTheme: BaseTheme = {
     color: AirBnbRawTheme.colorLevel1,
     border: 'none',
     activeBackgroundColor: AirBnbRawTheme.brandColor2,
-    activeBorder: 'none'
+    activeBorder: 'none',
   },
   /**
    * DROPDOWN
@@ -138,9 +186,8 @@ export const DarkTheme: BaseTheme = {
   /**
    * DOT
    */
-   dot: {
+  dot: {
     ...Base.dot,
-    backgroundColor: AirBnbRawTheme.colorLevel1
-  }
-}
-
+    backgroundColor: AirBnbRawTheme.colorLevel1,
+  },
+};
